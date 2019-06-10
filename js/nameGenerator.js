4dff959741a8ecec8a49c2a3477f7ed17f94664d
Brockton Sorensen <tin_soldier2008@yahoo.com>
2019-06-10 13:32:10 -0700

    Added milestone2.md

--- js/nameGenerator.js
+++ js/nameGenerator.js
@@ -23,7 +23,8 @@ function processing() {
     fnli = firstName.slice(first.length - 1,first.length);
     
     var middle = $('#middleName').val();
-    mnfi = middle.slice(0,1);
+    middleName = middle.toLowerCase();
+    mnfi = middleName.slice(0,1);
     
     var last = $('#lastName').val();
     lastName = last.toLowerCase();
@@ -255,8 +256,8 @@ switch(lnfi) {
         break;
         
 }
-$('.button').css('display','none');
-$('#output').html(output);
+    $('.button').css('display','none');
+    $('#output').html(output);
 }
 
 function batman() {
@@ -1241,8 +1242,8 @@ function redneck() {
     }    
 }
 output += ", toss me another beer!";
-$('.button').css('display','none');
-$('#output').html(output);  
+    $('.button').css('display','none');
+    $('#output').html(output);  
 }
 
 function bandit() {
@@ -1642,10 +1643,880 @@ switch (day) {
         output += 'the End';
         break;
 }
-$('.button').css('display','none');
-$('#output').html(output);
+    $('.button').css('display','none');
+    $('#output').html(output);
 }
 
 function unicorn() {
+    output += 'Candy and Rainbows, it\'s ';
+switch(fnfi) {
+    case 'a':
+        output += 'Glitter';
+        break;
 
+        
+    case 'b':
+        output += 'Rainbow';
+        break;
+
+    case 'c':
+        output += 'Twinkle';
+        break;
+
+    case 'd':
+        output += 'Happy';
+        break;
+
+    case 'e':
+        output += 'Bright';
+        break;
+
+    case 'f':
+        output += 'Shimmer';
+        break;
+
+    case 'g':
+        output += 'Dazzle';
+        break;
+
+    case 'h':
+        output += 'Princess';
+        break;
+
+    case 'i':
+        output += 'Lovely';
+        break;
+
+    case 'j':
+        output += 'Spirit';
+        break;
+
+    case 'k':
+        output += 'Joyful';
+        break;
+
+    case 'l':
+        output += 'Shiny';
+        break;
+
+    case 'm':
+        output += 'Elegant';
+        break;
+
+    case 'n':
+        output += 'Cute';
+        break;
+
+    case 'o':
+        output += 'Glitzy';
+        break;
+
+    case 'p':
+        output += 'Sparkly';
+        break;
+
+    case 'q':
+        output += 'Doom';
+        break;
+
+    case 'r':
+        output += 'Amazing';
+        break;
+
+    case 's':
+        output += 'Delightful';
+        break;
+
+    case 't':
+        output += 'Rosy';
+        break;
+
+    case 'u':
+        output += 'Moonbeam';
+        break;
+
+    case 'v':
+        output += 'Fancy';
+        break;
+
+    case 'w':
+        output += 'Sunshine';
+        break;
+
+    case 'x':
+        output += 'Starlight';
+        break;
+
+    case 'y':
+        output += 'Breezy';
+        break;
+
+    case 'z':
+        output += 'Pearly';
+        break;
+}
+    output += ' ';
+switch(month) {
+    case '01':
+        output += 'Nova';
+        break;
+
+        
+    case '02':
+        output += 'Breeze';
+        break;
+
+    case '03':
+        output += 'Horn';
+        break;
+
+    case '04':
+        output += 'Queen';
+        break;
+
+    case '05':
+        output += 'Fairy Dust';
+        break;
+
+    case '06':
+        output += 'Sparkle Hoof';
+        break;
+
+    case '07':
+        output += 'Song';
+        break;
+
+    case '08':
+        output += 'Shining Mane';
+        break;
+
+    case '09':
+        output += 'Star Dust';
+        break;
+
+    case '10':
+        output += 'Moonlight';
+        break;
+
+    case '11':
+        output += 'Comet Tail';
+        break;
+
+    case '12':
+        output += 'Sky Gallop';
+        break;
+}
+    output += '!';
+    $('.button').css('display','none');
+    $('#output').html(output);
+}
+
+function blues() {
+    output += 'You are the King of Blues, ';
+switch(fnfi) {
+    case 'a':
+        output += 'Fat';
+        break;
+
+        
+    case 'b':
+        output += 'Blind';
+        break;
+
+    case 'c':
+        output += 'Sticky';
+        break;
+
+    case 'd':
+        output += 'Old';
+        break;
+
+    case 'e':
+        output += 'Buddy';
+        break;
+
+    case 'f':
+        output += 'Howlin\'';
+        break;
+
+    case 'g':
+        output += 'Ugly';
+        break;
+
+    case 'h':
+        output += 'Beautiful';
+        break;
+
+    case 'i':
+        output += 'Happy';
+        break;
+
+    case 'j':
+        output += 'Cryin\'';
+        break;
+
+    case 'k':
+        output += 'Jailhouse';
+        break;
+
+    case 'l':
+        output += 'Red';
+        break;
+
+    case 'm':
+        output += 'Rail Ridin\'';
+        break;
+
+    case 'n':
+        output += 'Rainy';
+        break;
+
+    case 'o':
+        output += 'Sleepy';
+        break;
+
+    case 'p':
+        output += 'Bald';
+        break;
+
+    case 'q':
+        output += 'Devil\'s';
+        break;
+
+    case 'r':
+        output += 'Skinny';
+        break;
+
+    case 's':
+        output += 'Big';
+        break;
+
+    case 't':
+        output += 'Tiny';
+        break;
+
+    case 'u':
+        output += 'Toothless';
+        break;
+
+    case 'v':
+        output += 'Singin\'';
+        break;
+
+    case 'w':
+        output += 'Steel-Eye';
+        break;
+
+    case 'x':
+        output += 'Stinky';
+        break;
+
+    case 'y':
+        output += 'Deaf';
+        break;
+
+    case 'z':
+        output += 'Godly';
+        break;
+}
+    output += ' ';
+switch(mnfi) {
+    case 'a':
+        output += 'Bones';
+        break;
+
+        
+    case 'b':
+        output += 'Man';
+        break;
+
+    case 'c':
+        output += 'Guitar';
+        break;
+
+    case 'd':
+        output += 'Legs';
+        break;
+
+    case 'e':
+        output += 'Hands';
+        break;
+
+    case 'f':
+        output += 'Eyes';
+        break;
+
+    case 'g':
+        output += 'Money';
+        break;
+
+    case 'h':
+        output += 'King';
+        break;
+
+    case 'i':
+        output += 'Gumbo';
+        break;
+
+    case 'j':
+        output += 'Po\' Boy';
+        break;
+
+    case 'k':
+        output += 'Hurricane';
+        break;
+
+    case 'l':
+        output += 'Fingers';
+        break;
+
+    case 'm':
+        output += 'VooDoo';
+        break;
+
+    case 'n':
+        output += 'Feet';
+        break;
+
+    case 'o':
+        output += 'Mama';
+        break;
+
+    case 'p':
+        output += 'Dog';
+        break;
+
+    case 'q':
+        output += 'Gator';
+        break;
+
+    case 'r':
+        output += 'Skunk';
+        break;
+
+    case 's':
+        output += 'Tooth';
+        break;
+
+    case 't':
+        output += 'Smoke';
+        break;
+
+    case 'u':
+        output += 'Pickles';
+        break;
+
+    case 'v':
+        output += 'Baby';
+        break;
+
+    case 'w':
+        output += 'Sugar';
+        break;
+
+    case 'x':
+        output += 'Death';
+        break;
+
+    case 'y':
+        output += 'Eggs';
+        break;
+
+    case 'z':
+        output += 'Zoot';
+        break;
+
+
+}
+    output += ' '
+switch(lnfi) {
+    case 'a':
+        output += 'Jones';
+        break;
+
+        
+    case 'b':
+        output += 'Jackson';
+        break;
+
+    case 'c':
+        output += 'McGee';
+        break;
+
+    case 'd':
+        output += 'Tuesday';
+        break;
+
+    case 'e':
+        output += 'Green';
+        break;
+
+    case 'f':
+        output += 'Rivers';
+        break;
+
+    case 'g':
+        output += 'Malone';
+        break;
+
+    case 'h':
+        output += 'Dupree';
+        break;
+
+    case 'i':
+        output += 'Smith';
+        break;
+
+    case 'j':
+        output += 'Parker';
+        break;
+
+    case 'k':
+        output += 'White';
+        break;
+
+    case 'l':
+        output += 'Walker';
+        break;
+
+    case 'm':
+        output += 'Lee';
+        break;
+
+    case 'n':
+        output += 'King';
+        break;
+
+    case 'o':
+        output += 'Jefferson';
+        break;
+
+    case 'p':
+        output += 'Davis';
+        break;
+
+    case 'q':
+        output += 'Franklin';
+        break;
+
+    case 'r':
+        output += 'Jenkins';
+        break;
+
+    case 's':
+        output += 'Love';
+        break;
+
+    case 't':
+        output += 'Johnson';
+        break;
+
+    case 'u':
+        output += 'Allison';
+        break;
+
+    case 'v':
+        output += 'Cooper';
+        break;
+
+    case 'w':
+        output += 'Cobbler';
+        break;
+
+    case 'x':
+        output += 'Brown';
+        break;
+
+    case 'y':
+        output += 'Hawkins';
+        break;
+
+    case 'z':
+        output += 'Bailey';
+        break;
+}
+    output += '.';
+    $('.button').css('display','none');
+    $('#output').html(output);
+}
+
+function dragon() {
+    var first = $('#firstName').val();
+    var middle = $('#middleName').val();
+    middle = middle.toLowerCase();
+    var last = $('#lastName').val();
+    last = last.toLowerCase();
+    var a = first.slice(first.length - 2, first.length - 1);
+    a = a.toUpperCase();
+    var b = first.slice(first.length - 1 , first.length);
+    var x = Math.floor(last.length/2);
+    var c = last.slice(x - 1 , x + 1);
+    var d = middle.slice(0 , 2);
+    var e = last.slice(0 , 1);
+    output = `The great wyrm, ${a}${b}${c}${d}${e}, awakens`;
+    $('.button').css('display','none');
+    $('#output').html(output);
+}
+
+function pokemon() {
+var type1 = '';
+var type2 = '';
+var a = '-';
+switch(month) {
+    case '01':
+        type1 = 'Ice';
+        break;
+        
+    case '02':
+        type1 = 'Normal';
+        break;
+        
+    case '03':
+        type1 = 'Water';
+        break;
+        
+    case '04':
+        type1 = 'Electric';
+        break;
+        
+    case '05':
+        type1 = 'Psychic';
+        break;
+        
+    case '06':
+        type1 = 'Bug';
+        break;
+        
+    case '07':
+        type1 = 'Fire';
+        break;
+        
+    case '08':
+        type1 = 'Grass';
+        break;
+        
+    case '09':
+        type1 = 'Dragon';
+        break;
+        
+    case '10':
+        type1 = 'Dark';
+        break;
+        
+    case '11':
+        type1 = 'Steel';
+        break;
+        
+    case '12':
+        type1 = 'Ghost';
+        break;
+        
+        
+}
+switch(day) {
+    case '01':
+        type2 = 'Psychic';
+        break;
+        
+    case '02':
+        type2 = 'Fire';
+        break;
+    case '03':
+        type2 = 'Ice';
+        break;
+    case '04':
+        type2 = 'Electric';
+        break;
+    case '05':
+        type2 = 'Dragon';
+        break;
+    case '06':
+        type2 = 'Grass';
+        break;
+    case '07':
+        type2 = 'Steel';
+        break;
+    case '08':
+        type2 = 'Rock';
+        break;
+    case '09':
+        type2 = 'Water';
+        break;
+    case '10':
+        type2 = 'Normal';
+        break;
+    case '11':
+        type2 = 'Fighting';
+        break;
+    case '12':
+        type2 = 'Ghost';
+        break;
+    case '13':
+        type2 = 'Flying';
+        break;
+    case '14':
+        type2 = 'Flying';
+        break;
+    case '15':
+        type2 = 'Flying';
+        break;
+    case '16':
+        type2 = 'Flying';
+        break;
+    case '17':
+        type2 = 'Poison';
+        break;
+    case '18':
+        type2 = 'Ground';
+        break;
+    case '19':
+        type2 = 'Dark';
+        break;
+    case '20':
+        type2 = 'Fighting';
+        break;
+    case '21':
+        type2 = 'Psychic';
+        break;
+    case '22':
+        type2 = 'Dark';
+        break;
+    case '23':
+        type2 = 'Steel';
+        break;
+    case '24':
+        type2 = 'Dragon';
+        break;
+    case '25':
+        type2 = 'Fire';
+        break;
+    case '26':
+        type2 = 'Ice';
+        break;
+    case '27':
+        type2 = 'Electric';
+        break;
+    case '28':
+        type2 = 'Normal';
+        break;
+    case '29':
+        type2 = 'Poison';
+        break;
+    case '30':
+        type2 = 'Fighting';
+        break;
+    case '31':
+        type2 = 'Bug';
+        break;
+}
+
+if(type1 == type2) {
+    a = '';
+    type2 = '';
+}
+var base = $('#animal').val();
+var random = Math.floor(Math.random() * base.length/2);
+var name1 = base.slice(random , random + 1);
+name1 = name1.toUpperCase();
+var name2 = base.slice(random + 1, random + 4);
+var typerandom = Math.floor(Math.random() * 2);
+var name3 = '';
+switch(type1) {
+    case 'Ice':
+        switch (typerandom) {
+            case 0:
+                name3 = 'frost';
+                break;
+            
+            case 1:
+                name3 = 'chill';
+                break;
+
+            case 2:
+                name3 = 'snow';
+
+        }
+        break;
+        
+    case 'Normal':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'khan';
+                    break;
+                
+                case 1:
+                    name3 = 'don';
+                    break;
+    
+                case 2:
+                    name3 = 'therium';
+    
+            }
+        break;
+
+    case 'Water':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'deep';
+                    break;
+                
+                case 1:
+                    name3 = 'flow';
+                    break;
+    
+                case 2:
+                    name3 = 'rain';
+    
+            }
+        break;
+
+    case 'Electric':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'zap';
+                    break;
+                
+                case 1:
+                    name3 = 'bolt';
+                    break;
+    
+                case 2:
+                    name3 = 'buzz';
+    
+            }
+        break;
+
+    case 'Psychic':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'star';
+                    break;
+                
+                case 1:
+                    name3 = 'esp';
+                    break;
+    
+                case 2:
+                    name3 = 'kine';
+    
+            }
+        break;
+
+    case 'Bug':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'bee';
+                    break;
+                
+                case 1:
+                    name3 = 'shell';
+                    break;
+    
+                case 2:
+                    name3 = 'horn';
+    
+            }
+        break;
+
+    case 'Fire':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'burn';
+                    break;
+                
+                case 1:
+                    name3 = 'char';
+                    break;
+    
+                case 2:
+                    name3 = 'blaze';
+    
+            }
+        break;
+
+    case 'Grass':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'ish';
+                    break;
+                
+                case 1:
+                    name3 = 'eed';
+                    break;
+    
+                case 2:
+                    name3 = 'root';
+    
+            }
+        break;
+
+    case 'Dragon':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'don';
+                    break;
+                
+                case 1:
+                    name3 = 'ite';
+                    break;
+    
+                case 2:
+                    name3 = 'king';
+    
+            }
+        break;
+
+    case 'Dark':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'shade';
+                    break;
+                
+                case 1:
+                    name3 = 'moon';
+                    break;
+    
+                case 2:
+                    name3 = 'claw';
+    
+            }
+        break;
+
+    case 'Steel':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'ite';
+                    break;
+                
+                case 1:
+                    name3 = 'ium';
+                    break;
+    
+                case 2:
+                    name3 = 'tine';
+    
+            }
+        break;
+
+    case 'Ghost':
+            switch (typerandom) {
+                case 0:
+                    name3 = 'ade';
+                    break;
+                
+                case 1:
+                    name3 = 'tre';
+                    break;
+    
+                case 2:
+                    name3 = 'haunt';
+    
+            }
+        break;
+}
+
+    var name = name1 + name2 + name3;
+    output = `${name} is a ${type1}${a}${type2} type Pokemon`
+    $('.button').css('display','none');
+    $('#output').html(output);
 }
